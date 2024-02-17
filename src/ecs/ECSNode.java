73b88a7136e4fcd1881fa1d239833bd4aaef5ab4
da.patel@mail.utoronto.ca <da.patel@mail.utoronto.ca>
2024-02-17 16:55:31 -0500

    update  ECSNode

--- src/ecs/ECSNode.java
+++ src/ecs/ECSNode.java
@@ -1,9 +1,30 @@
 package ecs;
 
+import app_kvServer.KVServer;
+
 public class ECSNode implements IECSNode {
+    private KVServer kvServer;
+    private String nodeName;
+    private String nodeHost;
+    private int nodePort;
+    private String[] nodeHashRange;
+    private int cacheSize;
+    private String dBStoragePath;
+    private String strategy;
 
-    void IECSNode(){
 
+    public ECSNode(String nodeName, String nodeHost, int nodePort, String[] nodeHashRange, int cacheSize, String dBStoragePath, String strategy) {
+        this.nodeName = nodeName;
+        this.nodeHost = nodeHost;
+        this.nodePort = nodePort;
+        this.nodeHashRange = nodeHashRange;
+        this.cacheSize = cacheSize;
+        this.dBStoragePath = dBStoragePath;
+        this.strategy = strategy;
+    }
+
+    public void startServer() {
+        kvServer = new KVServer(nodePort, cacheSize, strategy, nodeHost, dBStoragePath);
     }
     public String getNodeName(){
         return null;

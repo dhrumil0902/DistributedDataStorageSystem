8e50ac29777611b5aecd0b9766cb536767308332
da.patel@mail.utoronto.ca <da.patel@mail.utoronto.ca>
2024-02-24 11:02:35 -0500

    added ECSMessage to communicate between KVServer and ECSClient

---
+++ src/app_kvECS/ECSMessage.java
@@ -0,0 +1,78 @@
+package app_kvECS;
+import java.io.*;
+import java.util.*;
+
+public class ECSMessage implements Serializable {
+    public ActionType action;
+    public boolean success = false;
+    public List<String> data;
+
+    public String[] range;
+
+    public BST nodes;
+
+    public ECSMessage(ActionType action, boolean success, List<String> data, String[] range, BST nodes) {
+        this.action = action;
+        this.success = success;
+        this.data = data;
+        this.range = range;
+        this.nodes = nodes;
+    }
+}
+
+enum ActionType {
+    DELETE, //stop the server (keeps running but returns SERVER_STOPPED to client) returns (success = true)
+    SET_WRITE_LOCK, // sets write lock, returns success = true
+    UNSET_WRITE_LOCK, //unset write lock, return sucess = true
+    GET_ALL, // returns all the data, sucess= true , data has the list of KV pairs
+    APPEND, // appends the given KV pairs stored in "data" field, return successs = true
+    REMOVE, // remove all keys in the range field, and return those KV pairs, stored in data. success = true
+    UPDATE_METADATA // "nodes" field is filled with metadata, simply update kvserver's "nodes" field and return success = true
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        // Example data - JUST FOR TESTING PURPOSES
+        ActionType action = ActionType.SET_WRITE_LOCK;
+        boolean success = true;
+        List<String> dataList = Arrays.asList("data1", "data2", "data3");
+
+        ECSMessage ecsMessage = new ECSMessage(action, success, dataList, null, null);
+
+        String serializedString = serializeToString(ecsMessage);
+        System.out.println("Serialized String: " + serializedString);
+
+        // Deserialize string
+        ECSMessage deserializedMessage = deserializeFromString(serializedString);
+        System.out.println("Action: " + deserializedMessage.action + ", Success: " + deserializedMessage.success);
+
+        for (String item : deserializedMessage.data) {
+            System.out.println("Data: " + item);
+        }
+    }
+
+    static String serializeToString(Object obj) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.close();
+            return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    static ECSMessage deserializeFromString(String serializedString) {
+        try {
+            byte[] data = Base64.getDecoder().decode(serializedString);
+            ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data));
+            return (ECSMessage) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

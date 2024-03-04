b597e27f6d064ca802b27a23a3754d98870845b1
da.patel@mail.utoronto.ca <da.patel@mail.utoronto.ca>
2024-03-03 21:39:46 -0500

    added performance tests

---
+++ src/testing/PerformanceTest.java
@@ -0,0 +1,95 @@
+package testing;
+
+import app_kvECS.ECSClient;
+import app_kvServer.KVServer;
+import client.KVStore;
+import logger.LogSetup;
+import org.apache.log4j.Level;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PerformanceTest {
+    private static AtomicInteger putCount = new AtomicInteger(0);
+
+    public static void main(String[] args) throws Exception {
+        new LogSetup("logs/testing/test2.log", Level.OFF);
+        ECSClient ecsClient = new ECSClient("localhost", 5201);
+    }
+
+    private static void runPerformanceTest(double putRatio, int totalRequests, int numServers) throws Exception {
+
+        for (int num = 1; num <= numServers; num++){
+            KVServer kvServer = new KVServer("localhost", 5201, "localhost", 5000 + (8 * num), 0, "None", System.getProperty("user.dir"));
+
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Double> randomValues = generateRandomValues(totalRequests);
+        double getRatio = 1 - putRatio;
+
+        int getCount = 0;
+
+        long startTime = System.currentTimeMillis();
+        List<Future<Void>> futures = new ArrayList<>();
+
+        for (int i = 0; i < totalRequests; i++) {
+            final double randomValue = randomValues.get(i);
+            final double finalPutRatio = putRatio;
+
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    KVStore kvStore = new KVStore("localhost", 50000);
+                    try {
+                        kvStore.connect();
+
+                        if (randomValue < finalPutRatio) {
+                            kvStore.put("some_key" + randomValue, "some_value");
+                            putCount.incrementAndGet();
+                        } else {
+                            kvStore.get("some_key");
+                        }
+
+                        kvStore.disconnect();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            futures.add(CompletableFuture.runAsync(task, executorService));
+        }
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allOf.join();
+        long endTime = System.currentTimeMillis();
+
+        double totalSeconds = (endTime - startTime) / 1000.0;
+        double latency = totalSeconds / totalRequests;
+        double throughput = totalRequests / totalSeconds;
+
+        System.out.println("Latency: " + latency);
+        System.out.println("GetCount: " + getCount);
+        System.out.println("PutCount: " + putCount);
+        System.out.println("Throughput: " + throughput);
+
+        executorService.shutdown();
+    }
+
+    private static List<Double> generateRandomValues(int totalRequests) {
+        List<Double> randomValues = new ArrayList<>();
+
+        for (int i = 0; i < totalRequests; i++) {
+            randomValues.add(ThreadLocalRandom.current().nextDouble());
+        }
+
+        return randomValues;
+    }
+}
